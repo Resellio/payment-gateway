56d3b21be3cd630b8170142a2ac65601a25b8796
kTrzcinskii <kt.trzcinski@gmail.com>
2025-04-13 23:09:36 +0200

    Add cors and logger

--- src/common/config.rs
+++ src/common/config.rs
@@ -2,6 +2,7 @@ use std::env;
 
 pub struct Config {
     pub port: u16,
+    pub api_origin: String,
 }
 
 const DEAFULT_PORT: u16 = 8080;
@@ -12,6 +13,7 @@ impl Config {
             .ok()
             .and_then(|p| p.parse::<u16>().ok())
             .unwrap_or(DEAFULT_PORT);
-        Config { port }
+        let api_origin = env::var("API_ORIGIN").expect("missing 'API_ORIGIN' env");
+        Config { port, api_origin }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 mod common;
 mod health;
 
+use actix_cors::Cors;
 use actix_web::{
     App, HttpServer,
-    middleware::{NormalizePath, TrailingSlash},
+    middleware::{Logger, NormalizePath, TrailingSlash},
 };
 use common::config::Config;
+use env_logger::Env;
 use health::scopes::health_scope;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = Config::load_from_env();
-    HttpServer::new(|| {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin(&config.api_origin)
+            .allowed_methods(vec!["GET"]);
         App::new()
             .wrap(NormalizePath::new(TrailingSlash::Always))
+            .wrap(cors)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .service(health_scope())
     })
     .bind(("127.0.0.1", config.port))?
