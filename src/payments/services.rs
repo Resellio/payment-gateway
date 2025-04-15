cdf0ea610fc6278bffd759e3b82a4a75cc2ff6d6
kTrzcinskii <kt.trzcinski@gmail.com>
2025-04-15 20:27:24 +0200

    WIP: endpoint logic

--- src/payments/services.rs
+++ src/payments/services.rs
@@ -1,8 +1,17 @@
-use actix_web::{Responder, post, web};
+use actix_web::{http::StatusCode, post, web};
 
-use crate::payments::models::ProcessPaymentRequest;
+use crate::{
+    common::models::{AppResult, ErrorResponse},
+    payments::models::{ProcessPaymentRequest, ProcessPaymentSuccessResponse},
+};
 
 #[post("/")]
-async fn process_payment(request: web::Json<ProcessPaymentRequest>) -> impl Responder {
-    format!("Card number: {}", request.card_number)
+async fn process_payment(
+    request: web::Json<ProcessPaymentRequest>,
+) -> AppResult<ProcessPaymentSuccessResponse> {
+    if request.amount == 2.0 {
+        return Err(ErrorResponse::new("test".into(), StatusCode::BAD_REQUEST));
+    }
+    let result = ProcessPaymentSuccessResponse::success("123".into());
+    Ok(result)
 }
