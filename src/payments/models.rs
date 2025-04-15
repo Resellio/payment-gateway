de9c55f3ee3682d72332a1436a41f4009546fc2f
kTrzcinskii <kt.trzcinski@gmail.com>
2025-04-15 20:26:33 +0200

    Create response struct for payments endpoint

--- src/payments/models.rs
+++ src/payments/models.rs
@@ -1,3 +1,4 @@
+use actix_web::{HttpResponse, Responder, body::BoxBody, http::header::ContentType};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -8,4 +9,30 @@ pub struct ProcessPaymentRequest {
     pub(super) card_expiry: String,
     pub(super) cvv: String,
     pub(super) force_error: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ProcessPaymentSuccessResponse {
+    pub(super) transaction_id: String,
+    pub(super) status: String,
+}
+
+impl ProcessPaymentSuccessResponse {
+    pub fn success(transaction_id: String) -> Self {
+        Self {
+            transaction_id,
+            status: "success".into(),
+        }
+    }
+}
+
+impl Responder for ProcessPaymentSuccessResponse {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
 }
